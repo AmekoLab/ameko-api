e232732193e68acfa8ad49555ef4650afc5f8168
TinHT <cinhtcv@gmail.com>
2026-02-05 10:01:38 +0700

    TASK022 - Enable Swagger UI in all environments

--- FPTU.Capstone.AMKCollective.Api/Program.cs
+++ FPTU.Capstone.AMKCollective.Api/Program.cs
@@ -140,13 +140,13 @@ internal class Program
 
         var app = builder.Build();
 
-        if (app.Environment.IsDevelopment())
-        {
+        // if (app.Environment.IsDevelopment())
+        // {
             app.UseSwagger();
             app.UseSwaggerUI();
 
             app.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
-        }
+        // }
 
         app.UseHttpsRedirection();
         app.UseAuthentication();
