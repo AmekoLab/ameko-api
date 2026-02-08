02883cf0c51e5aec9bb30a38888d118b5a1a96e0
TinHT <cinhtcv@gmail.com>
2026-02-08 21:08:56 +0700

    TASK014 - Add ShopId to Category, unify soft delete handling

---
+++ FPTU.Capstone.AMKCollective.Infrastructure/Migrations/20260208140513_AddShopIdToCategory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPTU.Capstone.AMKCollective.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShopIdToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_IsDelete",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Categories",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ShopId",
+                table: "Categories",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_IsDeleted",
+                table: "Categories",
+                column: "IsDeleted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_IsDeleted",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ShopId",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Categories",
+                type: "tinyint(1)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)",
+                oldDefaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "Categories",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_IsDelete",
+                table: "Categories",
+                column: "IsDelete");
+        }
+    }
+}
