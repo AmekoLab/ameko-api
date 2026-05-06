08a3e0aeb943f7f4b68c1342c8f3b9fcfd1f33e3
TinHT <cinitcv@gmail.com>
2026-05-06 20:49:07 +0700

    TASK014 - Increase Title length in CommunityPosts to 1000 chars

---
+++ FPTU.Capstone.AMKCollective.Infrastructure/Data/Migrations/20260506134556_UpdateNotificationAndCommunicationLengths.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPTU.Capstone.AMKCollective.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNotificationAndCommunicationLengths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "CommunityPosts",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "CommunityPosts",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldMaxLength: 1000)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
